fa05470eb35b258e62934a22be8f0c1d388c87ef
noagedo <gedonoa@gmail.com>
2023-06-20 15:59:01 +0300

    login for register user

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const { createRouter } = require("./routes/create");
 const { updateRouter } = require("./routes/update");
 const { getRouter } = require("./routes/get");
+const { signinRouter } = require("./routes/signin");
+
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(createRouter);
 app.use(updateRouter);
 app.use(getRouter);
+app.use(signinRouter);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
@@ -22,6 +25,9 @@ app.get("/admin", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/admin.html"));
   });
 
+  app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+  });
 const start = async () => {
   await mongoose.connect(
     "mongodb://127.0.0.1:27017/products" );
