49a094fd126a24cb28489e24338dd071b128d3e1
michalbara1 <michalbara1@gmail.com>
2023-06-20 14:01:08 +0300

    new project with manger side and home

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const { createRouter } = require("./routes/create");
+const { updateRouter } = require("./routes/update");
+const { getRouter } = require("./routes/get");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(createRouter);
+app.use(updateRouter);
+app.use(getRouter);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+  });
+
+app.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/admin.html"));
+  });
+
+const start = async () => {
+  await mongoose.connect(
+    "mongodb://127.0.0.1:27017/products" );
+
+  app.listen(3300, () => {
+    console.log("Servers up");
+  });
+};
+start();
---
+++ src/routes/create.js
@@ -0,0 +1,35 @@
+const { Product } = require("../models/product");
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const router = express.Router();
+const upload = multer({ dest: path.join(__dirname, "../public/img/") });
+
+router.post("/api/createProduct", async (req, res) => {
+  const { name, price, image } = req.body;
+  const isExist = await Product.findOne({ name });
+
+  if (isExist) {
+    return res.send("Item already exists");
+  }
+  const product = new Product({
+    name,
+    price,
+    image,
+   
+  });
+  await product.save();
+  res.send(product);
+});
+
+router.post("/api/upload", upload.single("file"), (req, res) => {
+  const fileName = req.file.filename + ".png";
+  const filePath = path.join(__dirname, "../public/img", fileName);
+  fs.renameSync(req.file.path, filePath);
+  res.send({ fileName });
+});
+
+
+exports.createRouter = router;
