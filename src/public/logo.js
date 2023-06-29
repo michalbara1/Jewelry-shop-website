08269911bc91cc96aae48e931a3656d9a90cd8e6
michalbara1 <michalbara1@gmail.com>
2023-06-30 00:36:05 +0300

    changed the footer, the canvas logo, the nav bar, added photos

--- src/public/logo.js
+++ src/public/logo.js
@@ -1,13 +1,13 @@
-
-//logo for the site
-const c = document.getElementById('myCanvas');
-const ctx = c.getContext('2d');
-var img = document.getElementById("logoImage");
-ctx.drawImage(img, 10, 10);
-ctx.font = "bold 40px Arial";
-ctx.textAllign='center';
-ctx.fillStyle= "#964B00";
-ctx.textBaeline='middle';
-const text='COCO Jewlery';
-ctx.fillText(text, c.width/2, c.height/2);
-
+// using canvas to write the logo
+document.addEventListener("DOMContentLoaded", function() {
+    const c = document.getElementById('myCanvas');
+    const ctx = c.getContext('2d');
+  
+    ctx.font = "bold 40px Arial";
+    ctx.textAlign = 'center';
+    ctx.fillStyle = "#964B00";
+    ctx.textBaseline = 'middle';
+    const text = 'COCO Jewelry';
+    ctx.fillText(text, c.width / 2, c.height / 2);
+  });
+  
